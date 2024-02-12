1e280950a9af709162cc78b26c2e510c2b98100c
asaf-ashwal <ashwalasafx@gmail.com>
2024-02-12 17:34:59 +0200

    api request

--- src/pages/Tests/Asaf.jsx
+++ src/pages/Tests/Asaf.jsx
@@ -3,12 +3,13 @@ import style from "../../components/TabSwitcher/style.module.css";
 import { Route, Routes } from 'react-router-dom';
 import Popup from '../../Context/Popup';
 import {PopupContext} from '../../Context/Popup';
+import apiReq from '../../functions/ApiReq';
 
 
 export default function Asaf() {
   const contex = useContext({...PopupContext});
   
-  
+ 
   return (
     <div style={{height: '100vh'}}>
 
