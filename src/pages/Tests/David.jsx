0a22e28466793de73e2583ab394d7dd9b8312fa2
davidshayo <davidshayoo@gmail.com>
2024-02-12 12:54:08 +0200

    create lead item

--- src/pages/Tests/David.jsx
+++ src/pages/Tests/David.jsx
@@ -3,10 +3,11 @@ import InputText from '../../components/InputText'
 import InputArea from '../../components/InputArea'
 import HeadLine from '../../components/HeadLine'
 import InputSearch from '../../components/InputSearch'
+import LeadItem from '../../components/LeadItem'
 
 
 export default function David() {
   return (
-    <InputSearch />
+    <LeadItem name={'david shayo'} to={'/david/2'} date={'12/09/2020'} email={'tst@tst'} />
   )
 }
