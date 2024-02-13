45ef4e817fa68d05593a21205010008567bfa728
asaf-ashwal <ashwalasafx@gmail.com>
2024-02-13 12:29:12 +0200

    loding animation

--- src/pages/CampaignPage/index.jsx
+++ src/pages/CampaignPage/index.jsx
@@ -9,10 +9,15 @@ import styles from "./style.module.css";
 import {createContext, useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import apiReq from "../../functions/ApiReq";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export const campaignContext = createContext(null);
 
 export default function campaignCampaignPage() {
+
+    const [toLoding, settoLoding] = useState(true);
+
+
   // get params
   const {id} = useParams();
 
@@ -21,11 +26,11 @@ export default function campaignCampaignPage() {
 
   
   const toget = async () => {
-    try {
+    try { settoLoding(true)
       let resolet = await apiReq({get: "get", path: `campaign/${id}`});
       setcampaign(resolet);
     } catch (error) {}
-    finally{}
+    finally{settoLoding(false)}
   };
   useEffect(() => {
     toget();
@@ -38,74 +43,82 @@ export default function campaignCampaignPage() {
         <div className={styles.holdTabs}>
           <div>
             <div className={styles.headLine}>
-              <HeadLine
-                primaryText={"מנויי חדר כושר גבעת שמואל"}
-                scontText={"24 מרשמים 3 הודעות"}
-                icone={<img src="/threePoints.svg" alt="three points" />}
-              />
-              <div className={styles.tabSwitcher}>
-                <TabSwitcher
-                  linkList={[
-                    {text: "נרשמים", href: "/campaign/123/leads/"},
-                    {text: "הודעות", href: "/campaign/141/messages/"},
-                  ]}
-                />
-              </div>
+                {toLoding? <ClipLoader
+        color={ '#0E9D85'}
+        loading={toLoding}
+        // cssOverride={override}
+        size={100}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />: <> <HeadLine
+      primaryText={"מנויי חדר כושר גבעת שמואל"}
+      scontText={"24 מרשמים 3 הודעות"}
+      icone={<img src="/threePoints.svg" alt="three points" />}
+    />
+    <div className={styles.tabSwitcher}>
+      <TabSwitcher
+        linkList={[
+          {text: "נרשמים", href: "/campaign/123/leads/"},
+          {text: "הודעות", href: "/campaign/141/messages/"},
+        ]}
+      />
+    </div>
 
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <LeadsTab
-                      LeadList={[
-                        {
-                          id: "33",
-                          name: "avior",
-                          email: "yuiad@asd.com",
-                          date: new Date(),
-                        },
-                        {
-                          id: "34",
-                          name: "dani",
-                          email: "tasdk@asd.com",
-                          date: new Date(),
-                        },
-                      ]}
-                    />
-                  }
-                />
-                <Route
-                  path="/leads/*"
-                  element={
-                    <LeadsTab
-                      LeadList={[
-                        {
-                          id: "33",
-                          name: "avior",
-                          email: "yuiad@asd.com",
-                          date: new Date(),
-                        },
-                        {
-                          id: "34",
-                          name: "dani",
-                          email: "tasdk@asd.com",
-                          date: new Date(),
-                        },
-                      ]}
-                    />
-                  }
-                />
-                <Route
-                  path="/messages/*"
-                  element={
-                    <ListMessage
-                      listMessage={[
-                        {id: 3124, name: "itamar", date: new Date()},
-                      ]}
-                    />
-                  }
-                />
-              </Routes>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <LeadsTab
+            LeadList={[
+              {
+                id: "33",
+                name: "avior",
+                email: "yuiad@asd.com",
+                date: new Date(),
+              },
+              {
+                id: "34",
+                name: "dani",
+                email: "tasdk@asd.com",
+                date: new Date(),
+              },
+            ]}
+          />
+        }
+      />
+      <Route
+        path="/leads/*"
+        element={
+          <LeadsTab
+            LeadList={[
+              {
+                id: "33",
+                name: "avior",
+                email: "yuiad@asd.com",
+                date: new Date(),
+              },
+              {
+                id: "34",
+                name: "dani",
+                email: "tasdk@asd.com",
+                date: new Date(),
+              },
+            ]}
+          />
+        }
+      />
+      <Route
+        path="/messages/*"
+        element={
+          <ListMessage
+            listMessage={[
+              {id: 3124, name: "itamar", date: new Date()},
+            ]}
+          />
+        }
+      />
+    </Routes></> }
+             
             </div>
           </div>
 
