ff21cd7f371ee53718904263b256cab92135f06b
IsraelMeirRosenberg <ymeirrozenberg@gmail.com>
2024-02-13 17:14:31 +0200

    i need to set the css of this componennt

--- src/components/PopOver/index.jsx
+++ src/components/PopOver/index.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
 import { useState } from 'react'
 import style from './style.module.css'
+import { Link } from 'react-router-dom'
 
 export default function PopOver({ children, listLinks }) {
     const [visibility, setVisibility] = useState()
-    return (
+    const [indexOpen, setIndexOpen] = useState({})
+    const handleClick = function (e) {
+        setVisibility(visibility === "show" ? "hide" : "show")
 
-        <button onClick={() => setVisibility(visibility === "show" ? "hide" : "show")}>
-            {children}
-            <div className={`${style.content} ${visibility === "show" ? style.show : visibility === "hide" ? style.noShow : ""}`}>
-                {visibility == null ? '' : ''}
+        const styles = {}
+        if (window.innerHeight - e.clientY > 400) {
+            styles["top"] = 20
+        }
+        else { styles["bottom"] = 20 }
+        if (window.innerWidth - e.clientX > 400) {
+            styles['left'] = 0
+        }
+        else {
+            styles['right'] = 0
+        }
+        styles["height"] = listLinks.length * 50
+        setIndexOpen(styles)
+    }
+
+
+    return (
+        <span className={style.btn}>
+            <button onClick={(e) => handleClick(e)}>
+                {children}
+            </button>
+            <div style={indexOpen} className={`${style.content} ${visibility === "show" ? style.show : visibility === "hide" ? style.noShow : ""}`}>
+                {visibility == null ? '' : listLinks.map(i => {
+                    return (
+                        i.href ?
+                            <Link to={i.href} key={i.name}>{i.icon} <span>{i.name}</span>  </Link>
+                            :
+                            <button onClick={i.onClick} key={i.name}>
+                                {i.icon}
+                                <span>{i.name}</span>
+                            </button>
+                    )
+                })}
             </div>
-        </button>
+        </span>
 
 
     )
--- src/pages/Tests/Israel.jsx
+++ src/pages/Tests/Israel.jsx
@@ -1,52 +1,26 @@
 import React from 'react'
 import PopOver from '../../components/PopOver'
 export default function Israel() {
-  const listRead = [{
-    id: '1234',
-    name: 'israel',
-    email: 'a@a',
-    date: new Date()
-  },
-  {
-    id: '5678',
-    name: 'israel',
-    email: 'a@a',
-    date: new Date()
-  }
-    ,
-  {
-    id: '9123',
-    name: 'israel',
-    email: 'a@a',
-    date: new Date()
-  }]
-  const listNoRead = [{
-    id: '1234',
-    name: 'israel',
-    email: 'a@a',
-    date: new Date()
-  },
-  {
-    id: '5678',
-    name: 'israel',
-    email: 'a@a',
-    date: new Date()
-  }
-    ,
-  {
-    id: '9123',
-    name: 'israel',
-    email: 'a@a',
-    date: new Date()
-  }]
-  const icon = <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M19.9201 15.05L13.4001 8.53001C12.6301 7.76001 11.3701 7.76001 10.6001 8.53001L4.08008 15.05" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  const iconchild = <svg width="4" height="18" viewBox="0 0 4 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 10C2.55228 10 3 9.55228 3 9C3 8.44772 2.55228 8 2 8C1.44772 8 1 8.44772 1 9C1 9.55228 1.44772 10 2 10Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M2 3C2.55228 3 3 2.55228 3 2C3 1.44772 2.55228 1 2 1C1.44772 1 1 1.44772 1 2C1 2.55228 1.44772 3 2 3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M2 17C2.55228 17 3 16.5523 3 16C3 15.4477 2.55228 15 2 15C1.44772 15 1 15.4477 1 16C1 16.5523 1.44772 17 2 17Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
+  const list = [{
+    name: 'editting',
+    href: '/',
+    icon: iconchild
+  },
+  {
+    name: "delete",
+    onClick: () => console.log('onclick is work'),
+    icon: iconchild
+  }]
 
   const tit = 'eikfemf'
   return (
     <div>
-      <PopOver list={listRead}>{icon}</PopOver>
+      <PopOver listLinks={list}>{iconchild}</PopOver>
 
     </div>
   )
