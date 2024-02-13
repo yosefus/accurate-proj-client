4db2f33fa90aacdcbcd6c9ef712805ad287d39d2
yosefus <yosalsoni@gmail.com>
2024-02-13 11:26:06 +0200

    fix

--- src/components/LeadItem/index.jsx
+++ src/components/LeadItem/index.jsx
@@ -11,7 +11,7 @@ export default function LeadItem({ to, name, date, email }) {
              to={`${to}`}>
                 <div className={`${styles.icon}`}>
                     <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3.5791 20C5.91489 17.5226 9.08612 16 12.5791 16C16.0721 16 19.2433 17.5226 21.5791 20M17.0791 7.5C17.0791 9.98528 15.0644 12 12.5791 12C10.0938 12 8.0791 9.98528 8.0791 7.5C8.0791 5.01472 10.0938 3 12.5791 3C15.0644 3 17.0791 5.01472 17.0791 7.5Z" stroke="#6B6B6B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M3.5791 20C5.91489 17.5226 9.08612 16 12.5791 16C16.0721 16 19.2433 17.5226 21.5791 20M17.0791 7.5C17.0791 9.98528 15.0644 12 12.5791 12C10.0938 12 8.0791 9.98528 8.0791 7.5C8.0791 5.01472 10.0938 3 12.5791 3C15.0644 3 17.0791 5.01472 17.0791 7.5Z" stroke="#6B6B6B" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg >
                 </div>
                 <div className={`${styles.desc}`}>
--- src/components/newMsg/index.jsx
+++ src/components/newMsg/index.jsx
@@ -7,7 +7,7 @@ return(
 
             <span>
             <svg width="11" height="10" viewBox="0 0 11 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.25049 1V9M1.25049 5H9.25049" stroke="#0E9D85" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.25049 1V9M1.25049 5H9.25049" stroke="#0E9D85" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 
             </span>
