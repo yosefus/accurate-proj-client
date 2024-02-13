f3d3ed82077c043aaa6cbb7cec2ebbdd325f9786
davidshayo <davidshayoo@gmail.com>
2024-02-13 11:08:56 +0200

    update css components

--- src/components/MessageForm/index.jsx
+++ src/components/MessageForm/index.jsx
@@ -22,10 +22,14 @@ const handleChange = e => setMassage(old => ({...old, [e.target.name]: e.target.
 
         <>
             <form onSubmit={handleSubmit} className={`${styles.massage}`}>
+                <div>
                 <Label primaryText={"שם ההודעה"} scontText={'שם פנימי שיהיה חשוף רק לך'} id='aa1122' required={true} />
                 <InputText type={'text'} name={'subject'} onChange={handleChange} value={massage.subject }/>
+                </div>
+                <div>
                 <Label primaryText={"הודעה"} scontText={'זוהי ההודעה שתשלח בתזמון הנבחר'} id='bb1122' />
                 <InputArea name={'content'} onChange={handleChange} value={massage.content} />
+                </div>
                 <div className={`${styles.btn}`} >
                     <Button text={'ביטול'} option={'2'} type='button' />
                     <Button text={'שמירה'} option={'1'} type="submit"/>
