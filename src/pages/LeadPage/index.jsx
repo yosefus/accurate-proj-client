cbd51613d41c114d097e35e71d576dd7db4e6675
Avi doitch <avdoitsh@gmail.com>
2024-02-13 18:27:28 +0200

    LeadPage

--- src/pages/LeadPage/index.jsx
+++ src/pages/LeadPage/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import styles from "./style.module.css"
 import { useParams } from 'react-router-dom'
+import HeadLine from '../../components/HeadLine'
 
 export default function LeadPage() {
     // const { param } = useParams();
@@ -8,16 +9,16 @@ export default function LeadPage() {
     // const lid = value.find(value=>)
     const data = new Date().toLocaleDateString('he-IL')
     const lid = {
-        first_name: "fgh",
-        last_name: "gfkfdjk",
-        phon_number: "gfdisl",
-        email: "fdjksfhdjsk",
-        remarks: "fhdsjkal",
+        first_name: "כגחדל",
+        last_name: "יעכחגלדך",
+        phon_number: "רוקן",
+        email: "עכמגצדשך עכיגחד",
+        remarks: "ענכגמדצ",
         Join_Date: data
     }
     return (
         <div className={styles.all}>
-            <div className={styles.big}>{lid.first_name}</div>
+            <div><HeadLine primaryText={lid.first_name + ' ' + lid.last_name} scontText={"פעיל"} icone={<img src="/edit-04.svg" alt="icon" />} /></div>
             <div className={styles.cub}>
                 <div className={styles.container}>
                     <div className={styles.item}>שם פרטי<p className={styles.big}>{lid.first_name}</p></div>
