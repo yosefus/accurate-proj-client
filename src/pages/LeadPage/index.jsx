e0b4875435ad0b79fc0805689c9a164ac3196d2c
Avi doitch <avdoitsh@gmail.com>
2024-02-14 11:43:15 +0200

    LeadPage

--- src/pages/LeadPage/index.jsx
+++ src/pages/LeadPage/index.jsx
@@ -2,35 +2,31 @@ import React, { useContext } from 'react'
 import styles from "./style.module.css"
 import { useParams } from 'react-router-dom'
 import HeadLine from '../../components/HeadLine'
+import { CampaignContext } from '../CampaignPage'
+import FormatDate from '../../functions/FormatDate'
 
 export default function LeadPage() {
-    // const { param } = useParams();
-    // const value = useContext(MyContext);
-    // const lid = value.find(value=>)
-    const data = new Date().toLocaleDateString('he-IL')
-    const lid = {
-        first_name: "כגחדל",
-        last_name: "יעכחגלדך",
-        phon_number: "רוקן",
-        email: "עכמגצדשך עכיגחד",
-        remarks: "ענכגמדצ",
-        Join_Date: data
-    }
+    const { leadId } = useParams();
+    const context = useContext(CampaignContext);
+    const campaign = context?.campaign || {}
+    const lid = campaign?.leads?.find?.(l => l.lead._id === leadId);
+    if (!lid) return <></>;
     return (
         <div className={styles.all}>
-            <div><HeadLine primaryText={lid.first_name + ' ' + lid.last_name} scontText={"פעיל"} icone={<img src="/edit-04.svg" alt="icon" />} /></div>
+            <div><HeadLine primaryText={lid.lead.name} scontText={"פעיל"} icone={<img src="/edit-04.svg" alt="icon" />} /></div>
             <div className={styles.cub}>
                 <div className={styles.container}>
-                    <div className={styles.item}>שם פרטי<p className={styles.big}>{lid.first_name}</p></div>
-                    <div className={styles.item}>שם משפחה<p className={styles.big}>{lid.last_name}</p></div>
-                    <div className={styles.item}>טלפון<p className={styles.big}>{lid.phon_number}</p></div>
-                    <div className={styles.item}>אימייל<p className={styles.big}>{lid.email}</p></div>
+                    <div className={styles.item}>שם <p className={styles.big}>{lid.lead.name}</p></div>
+                    <div className={styles.item}>טלפון<p className={styles.big}>{lid.lead.phone}</p></div>
+                    <div className={styles.item}>אימייל<p className={styles.big}>{lid.lead.email}</p></div>
                 </div>
                 <div>
-                    <div className={styles.item}>הערות<p className={styles.big}>{lid.remarks}</p></div>
+                    <div className={styles.item}>הערות<p className={styles.big}>{lid.lead.notes
+                    }</p></div>
+                    <br />
                 </div>
                 <div>
-                    <div className={`${styles.gray} ${styles.date}`}>תאריך הצטרפות : {lid.Join_Date.toString()}</div>
+                    <div className={`${styles.gray} ${styles.date}`}>תאריך הצטרפות : {FormatDate(lid.joinDate)}</div>
                 </div>
             </div>
             <div className={styles.bottom}>
