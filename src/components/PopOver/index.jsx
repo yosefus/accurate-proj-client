6851944820209dae3c66067743b29210df1c17f5
IsraelMeirRosenberg <ymeirrozenberg@gmail.com>
2024-02-14 12:07:12 +0200

    finisht the popover componennt

--- src/components/PopOver/index.jsx
+++ src/components/PopOver/index.jsx
@@ -6,26 +6,25 @@ import { Link } from 'react-router-dom'
 // listLinks = array of objects like {name,onClick or href, icon,color}
 // href = Link to which you want to send
 //onClick = A function you want to happen when clicked exmple () => console.log('onclick is work')
-// if you want color to link or button give color 
+// if you want color to link or button give just color green or red 
 export default function PopOver({ children, listLinks }) {
 
     const [visibility, setVisibility] = useState()
     const [indexOpen, setIndexOpen] = useState({})
     const handleClick = function (e) {
         setVisibility(visibility === "show" ? "hide" : "show")
-
+        const height = ` ${listLinks.length * 40 + 15}`
         const styles = {}
-        if (window.innerHeight - e.clientY > 400) {
-            styles["top"] = 30
+        if (window.innerHeight - e.clientY > height) {
+            styles["top"] = 43
         }
-        else { styles["bottom"] = 20 }
+        else { styles["bottom"] = 45 }
         if (window.innerWidth - e.clientX > 220) {
-            styles['left'] = 0
+            styles['left'] = -30
         }
         else {
             styles['right'] = 0
         }
-        const height = ` ${listLinks.length * 40 + 15}px`
 
         styles["height"] = height
         setIndexOpen(styles)
@@ -33,28 +32,31 @@ export default function PopOver({ children, listLinks }) {
 
 
     return (
-        <span className={style.iconmain}>
-            <button className={style.btnChild} onClick={(e) => handleClick(e)}>
-                {children}
-            </button>
-            <div style={indexOpen} className={` ${visibility === "show" ? style.show : visibility === "hide" ? style.noShow : ""}`}>
-                {visibility == null ? '' : listLinks.map(i => {
-                    return (
-                        <div key={i.name} className={style.content}>
+        <>
+            {visibility === "show" && <div onClick={() => setVisibility("hide")} className={style.layout} />}
+            <span className={style.iconmain}>
+                <button onClick={(e) => handleClick(e)} className={style.btnChild} >
+                    {children}
+                </button>
+                <div style={indexOpen} className={` ${visibility === "show" ? style.show : visibility === "hide" ? style.noShow : ""}`}>
+                    {visibility == null ? '' : listLinks.map(i => {
+                        return (
+                            <div key={i.name} className={`${style.content} ${style[i.color]}`}>
 
-                            {i.href ?
-                                <Link style={{ "color": i.color }} className={style.onelink} to={i.href} >{i.icon} <span>{i.name}</span>  </Link>
-                                :
-                                <button style={{ "color": i.color }} className={style.onelink} onClick={i.onClick} >
-                                    {i.icon}
-                                    <span>{i.name}</span>
-                                </button>
-                            }
-                        </div>
-                    )
-                })}
-            </div>
-        </span>
+                                {i.href ?
+                                    <Link className={`${style[i.color]} ${style.onelink}`} to={i.href} >{i.icon} <span>{i.name}</span>  </Link>
+                                    :
+                                    <button className={`${style[i.color]} ${style.onelink} `} onClick={i.onClick} >
+                                        {i.icon}
+                                        <span>{i.name}</span>
+                                    </button>
+                                }
+                            </div>
+                        )
+                    })}
+                </div>
+            </span>
+        </>
 
 
     )
--- src/pages/Tests/Israel.jsx
+++ src/pages/Tests/Israel.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import PopOver from '../../components/PopOver'
 export default function Israel() {
-  const iconchild = <svg width="4" height="18" viewBox="0 0 4 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 10C2.55228 10 3 9.55228 3 9C3 8.44772 2.55228 8 2 8C1.44772 8 1 8.44772 1 9C1 9.55228 1.44772 10 2 10Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M2 3C2.55228 3 3 2.55228 3 2C3 1.44772 2.55228 1 2 1C1.44772 1 1 1.44772 1 2C1 2.55228 1.44772 3 2 3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M2 17C2.55228 17 3 16.5523 3 16C3 15.4477 2.55228 15 2 15C1.44772 15 1 15.4477 1 16C1 16.5523 1.44772 17 2 17Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+  const iconchild = <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19.2505 17L18.2504 18.094C17.7199 18.6741 17.0006 19 16.2506 19C15.5006 19 14.7813 18.6741 14.2509 18.094C13.7197 17.5151 13.0005 17.1901 12.2507 17.1901C11.5009 17.1901 10.7816 17.5151 10.2505 18.094M1.25049 19H2.92503C3.41421 19 3.6588 19 3.88898 18.9447C4.09305 18.8957 4.28814 18.8149 4.46708 18.7053C4.66892 18.5816 4.84187 18.4086 5.18777 18.0627L17.7505 5.49998C18.5789 4.67156 18.5789 3.32841 17.7505 2.49998C16.9221 1.67156 15.5789 1.67156 14.7505 2.49998L2.18775 15.0627C1.84184 15.4086 1.66889 15.5816 1.54521 15.7834C1.43555 15.9624 1.35474 16.1574 1.30575 16.3615C1.25049 16.5917 1.25049 16.8363 1.25049 17.3255V19Z" stroke="#6B6B6B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
+
   const list = [{
     name: 'editting',
     href: '/',
     icon: iconchild,
-    color: 'yellow'
+    color: 'red'
   },
   {
     name: "delete",
