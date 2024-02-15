327fa6f726a6aa0684b43fcbebc2057f889d7c72
asaf-ashwal <ashwalasafx@gmail.com>
2024-02-15 12:17:39 +0200

    edit name of campaign

--- src/components/Campaign_Form/index.jsx
+++ src/components/Campaign_Form/index.jsx
@@ -5,13 +5,19 @@ import InputArea from '../InputArea';
 import InputText from '../InputText';
 import Label from '../Label';
 import { PopupContext } from '../../Context/Popup';
+import apiReq from '../../functions/ApiReq';
 
-export default function Campaign_Form({ campaign = {}, path }) {
+export default function Campaign_Form({ campaign = {}, path ,toget}) {
     const { closeDailog } = useContext(PopupContext)
     const [formData, setFormData] = useState({ title: campaign.title || "", details: campaign.details || "" })
-    const handleSubmit = (e) => {
+    // const [objtocheng, setobjtocheng] = useState({})
+    const handleSubmit = async(e) => {
         e.preventDefault()
-        console.log(formData);
+       if (campaign._id) {
+        let x= await apiReq({name:'put',path:`campaign/${campaign._id}`,data:formData})
+        toget()
+       } 
+        closeDailog()
     };
     const handleChange = (e) => {
         const nwe = e.target.value;
