eb3c11079f712263bbc16c205ee2946cbc4dba46
Avi doitch <avdoitsh@gmail.com>
2024-02-15 16:30:18 +0200

    height

--- src/components/LeadsTab/index.jsx
+++ src/components/LeadsTab/index.jsx
@@ -1,28 +1,30 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import FormatDate from "../../functions/FormatDate";
 import LeadItem from "../LeadItem";
 import InputSearch from "../InputSearch";
 import styles from "./style.module.css";
 import { useState } from "react";
-export default function LeadsTab({LeadList, idCampaign}) {
+export default function LeadsTab({ LeadList, idCampaign }) {
   // [{name:"",date:"",email:"",id:""},{}]
-const [searchToPrint, setsearchToPrint] = useState(LeadList)
-const hendelChenge=(e)=>setsearchToPrint (LeadList.filter(v=>
-    v.lead.Fname.includes(e.target.value) || v.lead.Lname.includes(e.target.value) ))
+  const [searchToPrint, setsearchToPrint] = useState(LeadList)
+  const hendelChenge = (e) => setsearchToPrint(LeadList.filter(v =>
+    v.lead.Fname.includes(e.target.value) || v.lead.Lname.includes(e.target.value)))
   return (
     <>
-      <InputSearch onChange={hendelChenge}/>
-      {searchToPrint.map((i) => {
-        return (
-          <LeadItem
-            key={i._id}
-            date={FormatDate(i?.joinDate)}
-            to={`/campaign/${idCampaign}/leads/${i.lead._id}`}
-            email={i?.lead?.email}
-            name={`${i?.lead?.Fname} - ${i?.lead?.Lname}`}
-          />
-        );
-      })}
+      <InputSearch onChange={hendelChenge} />
+      <div className={styles.all}>
+        {searchToPrint.map((i) => {
+          return (
+            <LeadItem
+              key={i._id}
+              date={FormatDate(i?.joinDate)}
+              to={`/campaign/${idCampaign}/leads/${i.lead._id}`}
+              email={i?.lead?.email}
+              name={`${i?.lead?.Fname} - ${i?.lead?.Lname}`}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
